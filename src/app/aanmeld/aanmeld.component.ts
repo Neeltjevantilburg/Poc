40175792c6f1f7e9b47960f83c9470cdbb056702
Neeltjevantilburg <neeltjevantilburg@hotmail.com>
2022-01-05 13:42:15 +0100

    Componenten aangemaakt

---
+++ src/app/aanmeld/aanmeld.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-aanmeld',
+  templateUrl: './aanmeld.component.html',
+  styleUrls: ['./aanmeld.component.sass']
+})
+export class AanmeldComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AanmeldComponent } from './aanmeld/aanmeld.component';
+import { DialogComponent } from './dialog/dialog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    AanmeldComponent,
+    DialogComponent
   ],
   imports: [
     BrowserModule,
